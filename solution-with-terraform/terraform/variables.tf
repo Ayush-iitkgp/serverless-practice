c9263905c6964e6aaa6a93dae469a50a3c3ab08b
Ayush Pandey <ayush.pandey@tier.app>
2024-02-14 17:22:31 +0100

    removed aws secrets

--- solution-with-terraform/terraform/variables.tf
+++ solution-with-terraform/terraform/variables.tf
@@ -5,9 +5,9 @@ variable "region" {
 }
 
 variable "access_key" {
-  default = "AKIAYS2NTNYMQCLJEXW3"
+  default = ""
 }
 
 variable "secret_key" {
-  default = "mYclm1WB53d29r4VUQQ6bc1VDmDp5NcfMomaca0a"
+  default = ""
 }
